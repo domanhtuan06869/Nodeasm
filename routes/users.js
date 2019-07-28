3801ab08c8307a50be8cc5d355707d0871fb5fac
Tuan <domanh.tuanpt@gmail.com>
2019-07-28 14:38:05 +0700

    fix resgitry

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const User = require('../models/user');
 // Load User model
 const { forwardAuthenticated } = require('../config/auth');
 
